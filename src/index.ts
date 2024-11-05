cd5f7556454641eb8c6a796ca58279b7708e6b1b
solebellsBEACH <lucassxxavier@gmail.com>
2024-11-05 18:14:46 -0300

    feat: adding swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
+import swaggerUi from 'swagger-ui-express';
+const swaggerFile = require('./swagger/swagger-output.json');
 
 dotenv.config();
 
@@ -8,8 +10,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log("API documentation: http://localhost:3000/docs");
 });
---
+++ src/swagger/swagger-output.json.d.ts
@@ -0,0 +1,4 @@
+declare module './swagger-output.json' {
+    const value: any;
+    export default value;
+}
