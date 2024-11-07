f5de42bd443502e5bf4d4adf66b366439c847ea3
solebellsBEACH <lucassxxavier@gmail.com>
2024-11-07 16:58:29 -0300

    feat: adding swagger assets

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import swaggerUi from 'swagger-ui-express';
 import express, { NextFunction, Request, Response } from 'express';
 import router from './routes';
+import cors from 'cors';
 import { swaggerOutput } from './swagger/swagger-output';
 
 const app = express();
@@ -9,7 +10,9 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript Node Express!');
 });
-;
+// CORS setup
+app.use(cors());
+
 // Swagger setup
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput));
 
